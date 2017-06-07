ab3c3cc8bb4e460c21c9a570a26fb6f6e244ab06
Niko <wohlgemuth.niko@gmail.com>
2017-06-07 17:15:23 +0200

    new text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { SiteSocketContainer } from '../shared/models/site-socket.model';
+import { Point } from '../shared/models/point.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @Output() updateCanvas = new EventEmitter<SiteSocketContainer>();
+
   constructor() { }
 
   ngOnInit() {
+      const text: string[] = [];
+      text.push('Hallo und Willkommen auf meiner Webseite!');
+      text.push('Mein Name ist Nikolai Wohlgemuth, ich bin Student und Software Entwickler und verbringe meine Freizeit, unter anderem, gerne mit dem Erstellen von Webseiten.');
+      const siteSocket = new SiteSocketContainer(text);
+      siteSocket.setPosition([new Point(0, 0), new Point(0, 200)], [500, 500]);
+      this.updateCanvas.emit(siteSocket);
   }
 
 }
